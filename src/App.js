e37a176ae9bd429a699726500d978fd664ae0512
Galiant <antonijodev@gmail.com>
2020-08-03 19:38:36 +0100

    Creating Home component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
+import Home from "./Home";
 
 import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Meeting Log</h1>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
