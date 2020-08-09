22fb0a4c9fd08785af052bdc8b3564d480f2b269
Galiant <antonijodev@gmail.com>
2020-08-09 21:48:06 +0100

    Using environment variables to protect API keys

--- src/Firebase.js
+++ src/Firebase.js
@@ -3,13 +3,13 @@ import "firebase/database";
 import "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDBAoVw-fJS5w5vRJoLxx-6X2MBbFe5jRA",
-  authDomain: "react-spa-7aedd.firebaseapp.com",
-  databaseURL: "https://react-spa-7aedd.firebaseio.com",
-  projectId: "react-spa-7aedd",
-  storageBucket: "react-spa-7aedd.appspot.com",
-  messagingSenderId: "695932645196",
-  appId: "1:695932645196:web:8143c99f183e487a24a9e9",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
