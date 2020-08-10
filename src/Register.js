a389868a959c131eb623724b00d3207336d90815
Galiant <antonijodev@gmail.com>
2020-08-10 21:15:10 +0100

    Working on Register component and managing state in forms

--- src/Register.js
+++ src/Register.js
@@ -1,16 +1,89 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React, { useState } from "react";
 
-const Register = ({ user }) => {
+const Register = () => {
+  const [displayName, setDisplayName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+
   return (
-    <div className="text-center mt-4">
-      <h1 className="text-primary">Register</h1>
-    </div>
+    <form className="mt-3">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-8">
+            <div className="card bg-light">
+              <div className="card-body">
+                <h3 className="font-weight-light mb-3">Register</h3>
+                <div className="form-row">
+                  <section className="col-sm-12 form-group">
+                    <label
+                      className="form-control-label sr-only"
+                      htmlFor="displayName"
+                    >
+                      Display Name
+                    </label>
+                    <input
+                      className="form-control"
+                      type="text"
+                      id="displayName"
+                      placeholder="Display Name"
+                      name="displayName"
+                      required
+                      value={displayName}
+                      onChange={(e) => setDisplayName(e.target.value)}
+                    />
+                  </section>
+                </div>
+                <section className="form-group">
+                  <label className="form-control-label sr-only" htmlFor="email">
+                    Email
+                  </label>
+                  <input
+                    className="form-control"
+                    type="email"
+                    id="email"
+                    placeholder="Email Address"
+                    required
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                </section>
+                <div className="form-row">
+                  <section className="col-sm-6 form-group">
+                    <input
+                      className="form-control"
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                  </section>
+                  <section className="col-sm-6 form-group">
+                    <input
+                      className="form-control"
+                      type="password"
+                      required
+                      name="repeatPassword"
+                      placeholder="Repeat Password"
+                      value={repeatPassword}
+                      onChange={(e) => setRepeatPassword(e.target.value)}
+                    />
+                  </section>
+                </div>
+                <div className="form-group text-right mb-0">
+                  <button className="btn btn-primary" type="submit">
+                    Register
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </form>
   );
-};
-
-Register.propTypes = {
-  user: PropTypes.string,
 };
 
 export default Register;
